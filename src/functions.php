614069eaa91b0f50b109632e83055b0587a9cc0c
Dean Joanidhi <deanj.development@gmail.com>
2023-01-07 11:24:52 +0200

    Finalized the game rules

--- src/functions.php
+++ src/functions.php
@@ -1,1 +1,16 @@
 <?php
+
+# Creation of a function that shows the rules of the game.
+function show_rules() {
+  echo "Welcome to the cheat card game!\n\n";
+  echo "Here are the rules:\n";
+  echo "1. The goal of the game is to get rid of all your cards before your opponent.\n";
+  echo "2. On your turn, you can play any card from your hand.\n";
+  echo "3. If you think your opponent is lying about the card they played, you can call their bluff by saying 'cheat'.\n";
+  echo "4. If you successfully catch your opponent in a bluff, they have to draw two extra cards.\n";
+  echo "5. If you are caught in a bluff, you have to draw one extra card.\n";
+  echo "6. The game ends when one player has no cards left. The player with no cards left wins.\n\n";
+  echo "Let's start the game!\n";
+}
+
+?>
