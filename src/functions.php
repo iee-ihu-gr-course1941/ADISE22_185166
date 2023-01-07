8d810660782331e6c1eabfc096f4e45652fe0a03
Dean Joanidhi <deanj.development@gmail.com>
2023-01-07 12:00:56 +0200

    Created several prerequisite functions

--- src/functions.php
+++ src/functions.php
@@ -13,4 +13,74 @@ function show_rules() {
   echo "Let's start the game!\n";
 }
 
+
+# Prerequisite Functions
+
+function updateDatabase($db, $cards) {
+    # We will first delete all the rows in the cards table
+    $sql = "DELETE FROM cards";
+    if (!$result = $db->query($sql)){
+        die('There was an error running the query [' . $db->error . ']');
+    }
+
+    # Adding new data to the database
+    $sql = "INSERT INTO cards (rank, suit, location) VALUES (?, ?, ?)";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param("sss", $rank, $suit, $location);
+
+    foreach ($cards as $card) {
+        $rank = $card['rank'];
+        $suit = $card['suit'];
+        $location = $card['location'];
+        $stmt->execute();
+    }
+
+    $stmt->close();
+}
+
+
+
+
+function shuffleCards() {
+    # We are taking the json file that contains the cards
+    # We then decode it
+    $json = file_get_contents('cards.json');
+    $cards = json_decode($json, true);
+
+    # We will shuffle the cards via the shuffle function
+    shuffle($cards);
+
+    # We finally return the shuffled deck of cards
+    return $cards;
+}
+
+
+
+
+
+
+#############################################
+
+
+
+
+
+# Game initialization
+function initializeGame() {
+    # Prior to dealing, we need to shuffle the cards
+    # We use the shuffleCards function to do that
+    $deck = shuffleCards();
+
+    # We deal the cards to the players
+    # We slice the deck in two halves
+    $player1Cards = array_slice($deck, 0, 26);
+    $player2Cards = array_slice($deck, 26);
+
+    # We update everything in the database with the updateDatabase function
+    updateDatabase($player1Cards, $player2Cards);
+
+
+}
+
+
 ?>
