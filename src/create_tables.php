5b6d1603fa72d6e9fc662a1ceabf59d7926561bb
Dean Joanidhi <deanj.development@gmail.com>
2023-01-07 12:00:05 +0200

    Changed comment letter

--- src/create_tables.php
+++ src/create_tables.php
@@ -6,17 +6,17 @@ $password = $DB_PASSWORD;
 $host = $DB_HOST;
 $dbname = $DB_NAME;
 
-// Connect to the database
+# Connect to the database
 $db = new mysqli($host,$username,$password,$dbname);
 
-// Error Check
+# Error Check
 if ($db->connect_errno > 0) {
     die('Connection Error: Unable to reach the database [' . $db->connect_error . ']');
 }
 
-/* Game End or Restart */
+## Game End or Restart ##
 
-// Checking if the tables exist before deleting them
+# Checking if the tables exist before deleting them
 $result = $db->query("SHOW TABLES LIKE 'players'");
 if ($result->num_rows > 0){
     $sql = "DROP TABLE players";
@@ -46,12 +46,12 @@ if ($result->num_rows > 0){
 
 echo "Tables have been deleted successfully!\n";
 
-/* Game Start */
+## Game Start ##
 
-// Checks if the tables exist already in the database
+# Checks if the tables exist already in the database
 $result = $db->query("SHOW TABLES LIKE 'players'");
 if ($result->num_rows == 0) {
-    // Creation of the players table to store player data
+    # Creation of the players table to store player data
     $sql = "CREATE TABLE IF NOT EXISTS players (
         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
@@ -66,7 +66,7 @@ if ($result->num_rows == 0) {
 
 $result = $db->query("SHOW TABLES LIKE 'games'");
 if ($result->num_rows == 0) {
-    // Creation of the games table to store game data
+    # Creation of the games table to store game data
     $sql = "CREATE TABLE IF NOT EXISTS games (
         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
         status VARCHAR(255) NOT NULL,
@@ -75,12 +75,12 @@ if ($result->num_rows == 0) {
         player_2_score INT NOT NULL
     )";
 
-    // Error catching
+    # Error catching
     if (!$result = $db->query($sql)) {
         die('There was an error running the query [' . $db->error . ']');
     }
 
-    // Delete rows in the games table that have a current_player value that refers to a row in the players table
+    # Delete rows in the games table that have a current_player value that refers to a row in the players table
     $sql = "DELETE FROM games WHERE current_player IN (SELECT id FROM players)";
     if (!$result = $db->query($sql)) {
         die('There was an error running the query [' . $db->error . ']');
@@ -89,7 +89,7 @@ if ($result->num_rows == 0) {
 
 $result = $db->query("SHOW TABLES LIKE 'cards'");
 if ($result->num_rows == 0) {
-    // Creation of the cards table to store everything about the cards
+    # Creation of the cards table to store everything about the cards
     $sql = "CREATE TABLE IF NOT EXISTS cards (
         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
         rank CHAR(2) NOT NULL,
